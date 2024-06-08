341989bb49cde10da696461aeb10e591eb572180
aldy-mx-bit <aldy5166@gmail.com>
2024-06-08 23:59:56 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,2 +1,12 @@
 // Create new server
 var express = require('express');
+var express = require('express');
+var router = express.Router();
+
+// Get comments page
+router.get('/', function(req, res) {
+    res.render('comments');
+});
+
+// Export module
+module.exports = router;
