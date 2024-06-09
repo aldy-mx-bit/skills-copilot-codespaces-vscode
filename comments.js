32a9f96427d66ade99eea4a7df8e0135cae43b33
aldy-mx-bit <aldy5166@gmail.com>
2024-06-09 00:24:41 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -4,22 +4,8 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const auth = require("../middleware/auth");
 
-// Bring in models
+// Import Post model
 const Post = require("../models/Post");
 const User = require("../models/User");
 const Comment = require("../models/Comment");
 
-// @route    POST /api/comments/:id
-// @desc     Comment on a post
-// @access   Private
-router.post(
-  "/:id",
-  [
-    auth,
-    check("text", "Text is required").not().isEmpty(),
-    check("name", "Name is required").not().isEmpty(),
-    ],
-    async (req, res) => {   
-    const errors = validationResult(req);
-    }
-);
