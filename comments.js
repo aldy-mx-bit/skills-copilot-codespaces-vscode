0ed8357b6daa2be9613b5630bb2631047f4878c0
aldy-mx-bit <aldy5166@gmail.com>
2024-06-09 00:22:47 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,7 +1,25 @@
 // Create new server
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const Comment = require('./models');
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../middleware/auth");
+
+// Bring in models
+const Post = require("../models/Post");
+const User = require("../models/User");
+const Comment = require("../models/Comment");
+
+// @route    POST /api/comments/:id
+// @desc     Comment on a post
+// @access   Private
+router.post(
+  "/:id",
+  [
+    auth,
+    check("text", "Text is required").not().isEmpty(),
+    check("name", "Name is required").not().isEmpty(),
+    ],
+    async (req, res) => {   
+    const errors = validationResult(req);
+    }
+);
