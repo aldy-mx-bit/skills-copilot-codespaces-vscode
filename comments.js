3f81fe81fbce0db5acc67f3903080fc6b710829c
aldy-mx-bit <aldy5166@gmail.com>
2024-06-09 00:16:27 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -4,4 +4,4 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const Comment = require('./models)
+const Comment = require('./models');
