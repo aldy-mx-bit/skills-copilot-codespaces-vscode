37fc268c952c00c002e9e76f909ca5eaa453f022
aldy-mx-bit <aldy5166@gmail.com>
2024-06-09 00:26:35 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,2 @@
 // Create new server
-const express = require("express");
-const router = express.Router();
-const { check, validationResult } = require("express-validator");
-const auth = require("../middleware/auth");
-
-// Import Post model
-const Post = require("../models/Post");
-const User = require("../models/User");
-const Comment = require("../models/Comment");
-
+console.log('skilsMember');
